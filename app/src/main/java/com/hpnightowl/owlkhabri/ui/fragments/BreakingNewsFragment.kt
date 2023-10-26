5d6f4bb43cc91ec8f22453abf8b2c9e9f5c66bb2
hpnightowl <iamhp2k@gmail.com>
2023-10-26 12:46:49 +0530

    [DNM]owlkhabri: making breaking news fragment work using model views and adapter

--- app/src/main/java/com/hpnightowl/owlkhabri/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/hpnightowl/owlkhabri/ui/fragments/BreakingNewsFragment.kt
@@ -5,11 +5,13 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.hpnightowl.owlkhabri.R
 import com.hpnightowl.owlkhabri.ui.NewsActivity
+import com.hpnightowl.owlkhabri.ui.adapters.Adapter
 import com.hpnightowl.owlkhabri.ui.ui.NewsViewModel
 
 class BreakingNewsFragment: Fragment(R.layout.breaking_news) {
     // Todo: adding comments later
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: Adapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
--- app/src/main/java/com/hpnightowl/owlkhabri/ui/ui/NewsViewModel.kt
+++ app/src/main/java/com/hpnightowl/owlkhabri/ui/ui/NewsViewModel.kt
@@ -1,10 +1,34 @@
 package com.hpnightowl.owlkhabri.ui.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hpnightowl.owlkhabri.ui.helpers.Resource
+import com.hpnightowl.owlkhabri.ui.models.NewsResponse
 import com.hpnightowl.owlkhabri.ui.repository.NewsRepository
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(
     val newsRepository: NewsRepository
 ): ViewModel() {
+    // Live data use for fragments that is subscribed to it
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val breakingNewsPage = 1 // temp made
 
+    // it will stay alive till our view model is alive Concept used Coroutine for network
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if(response.isSuccessful){
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
